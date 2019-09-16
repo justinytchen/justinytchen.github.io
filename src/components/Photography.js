f609a9ea5f05fbc4011e614733564232ddaef31c
Justin <justin99chen@gmail.com>
2019-09-15 23:39:24 -0700

    updated photography page

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -1,56 +1,51 @@
 import React, {Component} from 'react';
 import ImageGallery from 'react-image-gallery';
 import data from '../data.json';
+import Gallery from 'react-photo-gallery'
 
 export default class Photography extends Component {
     constructor(props) {
         super(props);
-        this.imageURLs = this.shuffle(data.photography);
-    }
+        this.state = {color: 'nocolor'};
+        this.imageURLs = data.photography;
+        for(var i in this.imageURLs ){
+            var curImage = this.imageURLs[i]
+            curImage.src = curImage.static
+            curImage.width = parseInt(curImage.width)
+            curImage.height = parseInt(curImage.height)
 
-    shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
-
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-
-            // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
         }
-
-        return array;
     }
 
-    renderItem(item) {
-        return (
-            <div className='image-gallery-image'>
-                <a href={item.flickrLink}>
-                <img src = {item.original}  alt = ""/>
-                </a>
-            </div>
-        );
+    mouseEntered(){
+        console.log("mouseEntered");
+        this.setState({
+            color: 'color'
+        });
+    }
+
+    mouseExited(){
+        console.log("mouseLeave");
+        this.setState({color: 'nocolor'});
     }
 
     render() {
-        var images = [];
-        for (var i in this.imageURLs) {
-            var staticUrl = this.imageURLs[i].static
-            var flickrUrl = this.imageURLs[i].flickr
-            images.push({
-                original: staticUrl,
-                flickrLink: flickrUrl,
-                renderItem: this.renderItem.bind(this)
-            });
-        }
-
+        console.log(this.state.color);
         return (
-            <ImageGallery items={images} showFullscreenButton={false} disableArrowKeys={true} showPlayButton={false}/>
+            <div
+                className={this.state.color}
+                onMouseEnter = {this.mouseEntered.bind(this)}
+                onMouseLeave = {this.mouseExited.bind(this)}
+            >
+                <Gallery
+                    photos={this.imageURLs}
+                    margin = {5}
+                    direction={'row'}
+                    targetRowHeight = {250}
+                />
+            </div>
+
         );
+
     }
 };
