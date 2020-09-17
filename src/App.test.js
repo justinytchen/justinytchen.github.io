eab689ac557da219aa70d66a5a949a0734b6c196
Justin Chen <justin99chen@gmail.com>
2020-09-17 03:18:47 -0700

    use env var in base url

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
