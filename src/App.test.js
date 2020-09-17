90b94dbc2afd5ccc00ae14238fb022352128e8d3
Justin Chen <justin99chen@gmail.com>
2020-09-17 03:31:44 -0700

    remove base name

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
